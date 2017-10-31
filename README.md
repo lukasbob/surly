3715f38fef30e62c1f175a4762a76dabed6515d2
Mads Vestergård Jacobsen <lukasbob@gmail.com>
2017-10-31 09:31:48 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# surly - marshaling for Go URLs
+
+So you want your URLs as JSON? we grudgingly comply.
+
+
---
+++ url.go
@@ -0,0 +1,66 @@
+// Package surly wraps std library's url to make it marshalable and unmarshalable to various formats
+package surly
+
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"net/url"
+)
+
+// URL is a wrapper for the url.URL type that allows json unmarshaling
+type URL struct {
+	*url.URL
+}
+
+func MustParse(rawurl string) *URL {
+	u, err := Parse(rawurl)
+	if err != nil {
+		panic(err)
+	}
+	return u
+}
+
+// Parse creates a new URL, delegating to url.Parse for parsing.
+func Parse(rawurl string) (*URL, error) {
+	u, err := url.Parse(rawurl)
+	if err == nil {
+		return New(u), err
+	}
+	return nil, err
+}
+
+// New creates a new URL
+func New(u *url.URL) *URL {
+	return &URL{URL: u}
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface
+func (u *URL) UnmarshalJSON(b []byte) error {
+	url, err := url.Parse(string(bytes.Trim(b, `"`)))
+	if err == nil {
+		u.URL = url
+	}
+	return err
+}
+
+// MarshalJSON implements the json.Marshaler interface
+func (u *URL) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf(`"%s"`, u.String())), nil
+}
+
+// UnmarshalXML immplements the xml.Unmarshaler interface
+func (u *URL) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var v string
+	d.DecodeElement(&v, &start)
+	url, err := url.Parse(v)
+	if err == nil {
+		u.URL = url
+	}
+	return err
+}
+
+// ResolveReference resolves a URI reference by delegating to the underlying url.URL's implementation
+func (u *URL) ResolveReference(other *URL) *URL {
+	return New(u.URL.ResolveReference(other.URL))
+}
