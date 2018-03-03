823e45aaaf5715a925f73f8e74cd45d3e3661445
Mads Vestergård Jacobsen <lukasbob@gmail.com>
2018-03-03 14:31:18 +0100

    Change approach: Type alias for string

--- url.go
+++ url.go
@@ -10,58 +10,46 @@ import (
 )
 
 // URL is a wrapper for the url.URL type that allows json unmarshaling
-type URL struct {
-	*url.URL
-}
+type URL string
 
-func MustParse(rawurl string) *URL {
-	u, err := Parse(rawurl)
+// New creates a new URL from a url.URL.
+func New(rawurl string) (URL, error) {
+	_, err := url.Parse(rawurl)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
-	return u
+	return URL(rawurl), nil
 }
 
-// Parse creates a new URL, delegating to url.Parse for parsing.
-func Parse(rawurl string) (*URL, error) {
-	u, err := url.Parse(rawurl)
-	if err == nil {
-		return New(u), err
-	}
-	return nil, err
-}
-
-// New creates a new URL
-func New(u *url.URL) *URL {
-	return &URL{URL: u}
+// Parsed returns url.URL representation of the URL string
+func (u URL) Parsed() *url.URL {
+	parsed, _ := url.Parse(string(u))
+	return parsed
 }
 
 // UnmarshalJSON implements the json.Unmarshaler interface
-func (u *URL) UnmarshalJSON(b []byte) error {
-	parsed, err := url.Parse(string(bytes.TrimSpace(bytes.Trim(b, `"`))))
-	if err == nil {
-		u.URL = parsed
-	}
+func (u URL) UnmarshalJSON(b []byte) error {
+	var err error
+	val := string(bytes.TrimSpace(bytes.Trim(b, `"`)))
+	u, err = New(val)
 	return err
 }
 
 // MarshalJSON implements the json.Marshaler interface
-func (u *URL) MarshalJSON() ([]byte, error) {
-	return []byte(fmt.Sprintf(`"%s"`, u.String())), nil
+func (u URL) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf(`"%s"`, u)), nil
 }
 
 // UnmarshalXML immplements the xml.Unmarshaler interface
-func (u *URL) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+func (u URL) UnmarshalXML(d *xml.Decoder, start xml.StartElement) (err error) {
 	var v string
 	d.DecodeElement(&v, &start)
-	parsed, err := url.Parse(strings.TrimSpace(v))
-	if err == nil {
-		u.URL = parsed
-	}
+	val := strings.TrimSpace(v)
+	u, err = New(val)
 	return err
 }
 
 // ResolveReference resolves a URI reference by delegating to the underlying url.URL's implementation
-func (u *URL) ResolveReference(other *URL) *URL {
-	return New(u.URL.ResolveReference(other.URL))
+func (u URL) ResolveReference(other URL) URL {
+	return URL(u.Parsed().ResolveReference(other.Parsed()).String())
 }
